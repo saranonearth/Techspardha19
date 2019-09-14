a5f62e23514fb1bf92cc660c5b3056f91f94b237
saranonearth <saran.suresh@outlook.com>
2019-09-14 15:40:56 +0530

    Updates

---
+++ precache-manifest.011b098d20f6f4abc336f006d2c6cd08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c1bf77f36e8ff7ed2462e507bbde6a",
+    "url": "/saranonearth/Techspardha19/index.html"
+  },
+  {
+    "revision": "d0215b7c30f3af8ccce6",
+    "url": "/saranonearth/Techspardha19/static/css/main.1200e793.chunk.css"
+  },
+  {
+    "revision": "e11e3b18241477eb0fbd",
+    "url": "/saranonearth/Techspardha19/static/js/2.e6c2db75.chunk.js"
+  },
+  {
+    "revision": "d0215b7c30f3af8ccce6",
+    "url": "/saranonearth/Techspardha19/static/js/main.d948970b.chunk.js"
+  },
+  {
+    "revision": "108b2a45950aef56f25e",
+    "url": "/saranonearth/Techspardha19/static/js/runtime~main.1214a9dd.js"
+  },
+  {
+    "revision": "ce1901dd31099a0d88ad653d509369a7",
+    "url": "/saranonearth/Techspardha19/static/media/grains.ce1901dd.png"
+  }
+]);
